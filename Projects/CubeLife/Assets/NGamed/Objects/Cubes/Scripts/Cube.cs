5c115b86fd4833d6b12b03c57f586aa9765f4ed9
Kirk <kirk@kirkwarez.com>
2019-04-29 02:40:20 +0200

    Some code, configured prefabs, no algorithm yet.

---
+++ Projects/CubeLife/Assets/NGamed/Objects/Cubes/Scripts/Cube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	public string[] frontConstraints;
+	public string[] rightConstraints;
+	public string[] backConstraints;
+	public string[] leftConstraints;
+
+
+	public bool isTransition() {
+		return frontConstraints.Length > 1 || rightConstraints.Length > 1 || backConstraints.Length > 1 || leftConstraints.Length > 1;
+	}
+}
---
+++ Projects/CubeLife/Assets/NGamed/Objects/Cubes/Scripts/Cubes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Cubes : MonoBehaviour {
+	public Cube[] cubePrefabs;
+
+
+	private void Start() {
+		
+	}
+}
